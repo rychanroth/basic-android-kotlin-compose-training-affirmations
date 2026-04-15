5578f58961225ef415a1c40322dcca5c2628dbc9
Ry Chanroth <rychanroth2009@gmail.com>
2026-04-15 10:06:29 +0700

    finished u3p1 activity 2

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -16,13 +16,38 @@
 package com.example.affirmations
 
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmations.data.Datasource
+import com.example.affirmations.model.Affirmation
 import com.example.affirmations.ui.theme.AffirmationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,6 +68,77 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+@Preview(showBackground = true)
+@Composable
+fun AffirmationApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+            .statusBarsPadding()
+            .safeDrawingPadding()
+    ) {
+        AffirmationList(
+            affirmationList = Datasource().loadAffirmations(),
+            modifier = Modifier.fillMaxSize()
+                .statusBarsPadding()
+        )
+    }
+
+}
+
 @Composable
 fun AffirmationsApp() {
+
 }
+
+@Composable
+fun AffirmationCard(
+    affirmation: Affirmation,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+    ) {
+        Column(
+        ) {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = stringResource(affirmation.stringResourceId),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.height(194.dp)
+                    .fillMaxWidth()
+            )
+            Text(
+                text = stringResource(affirmation.stringResourceId),
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.padding(8.dp)
+            )
+
+        }
+    }
+
+}
+
+
+@Composable
+fun AffirmationList(
+    affirmationList: List<Affirmation>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(affirmation, Modifier.padding(8.dp))
+
+        }
+    }
+
+}
+
+@Composable
+fun AffirmationCardPreview() {
+    val affirmatioin = Affirmation(R.string.affirmation1, R.drawable.image1)
+    AffirmationCard(affirmatioin)
+}
+
